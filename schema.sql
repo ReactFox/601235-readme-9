dc57e8fb74b19efec000376661fd3511bcd7b244
Boleslav <itboleslav@gmail.com>
2019-04-30 20:04:53 +0300

    Создаёт связи

--- schema.sql
+++ schema.sql
@@ -6,7 +6,7 @@ USE readme_db;
 
 CREATE TABLE users
 (
-    id       INT AUTO_INCREMENT PRIMARY KEY,
+    id       INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     dt_reg   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     email    CHAR(128) NOT NULL UNIQUE,
     name     CHAR(64)  NOT NULL,
@@ -17,66 +17,173 @@ CREATE TABLE users
 
 CREATE TABLE posts
 (
-    id         INT AUTO_INCREMENT PRIMARY KEY,
+    id         INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     dt_add     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    title      TEXT NOT NULL,
     content    TEXT(1024),
-    author_id  INT,
+    author_id  INT UNSIGNED,
     image      CHAR(128),
     video      CHAR(128),
     link       CHAR(128),
-    show_count INT
+    show_count INT UNSIGNED
 );
+
 
 CREATE TABLE comments
 (
-    id      INT AUTO_INCREMENT PRIMARY KEY,
+    id      INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     content TEXT(1024) NOT NULL
 );
 
 CREATE TABLE likes
 (
-    user_like INT NOT NULL,
-    post_like INT NOT NULL
+    id_like INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    user_like INT UNSIGNED NOT NULL,
+    post_like INT UNSIGNED NOT NULL
 );
 
 CREATE TABLE subscriber
 (
-    who_signed_id INT NOT NULL,
-    subscribed_id INT NOT NULL
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    who_signed_id INT UNSIGNED NOT NULL,
+    subscribed_id INT UNSIGNED NOT NULL
 );
 
 CREATE TABLE messages
 (
+    id_msg       INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     dt_add       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    cont_massage TEXT(1024) NOT NULL,
-    sender_id    INT        NOT NULL,
-    recipient    INT        NOT NULL
+    cont_massage TEXT(1024)   NOT NULL,
+    sender_id    INT UNSIGNED NOT NULL,
+    recipient    INT UNSIGNED NOT NULL
 );
+
 
 CREATE TABLE hashtags
 (
-    hashtag_id      INT AUTO_INCREMENT PRIMARY KEY,
+    hashtag_id      INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     hashtag_content TEXT(128)
 );
 
+
+
 CREATE TABLE content_type
 (
-    type_id    INT AUTO_INCREMENT PRIMARY KEY,
+    type_id    INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     title      CHAR(8) NOT NULL,
-    class_icon CHAR(5)
+    class_icon CHAR(5) NOT NULL
 );
 
-INSERT INTO content_type(title, class_icon)
-VALUES ('Текст', 'text');
+/*
+Создаёт связи
+автор: пользователь, создавший пост;
+тип контента: тип контента, к которому относится пост;
+*/
+
+SELECT u.id, u.name
+FROM users u
+         JOIN posts p
+              ON u.id = p.id;
+
+SELECT ct.type_id, ct.title, ct.class_icon
+FROM content_type ct
+         JOIN posts p
+              ON ct.type_id = p.id;
+
+
+SELECT p.id, name
+FROM posts p
+         JOIN hashtags ON p.id = hashtag_id;
+
+
+
+/*
+Вот не могу понять как правильно, как выше сделал или как ниже (((
+
+SELECT p.id, p.title, p.content
+FROM posts p
+         JOIN users u
+              ON p.id = u.id;
+
+SELECT p.id
+FROM posts p
+         JOIN content_type ct
+              ON p.id = ct.id;*/
+
+
+
+/*
+Связи:
+    автор: пользователь, создавший пост;
+    пост: пост, к которому добавлен комментарий.
+*/
+
+SELECT u.id, u.name
+FROM users u
+         JOIN comments c
+              ON u.id = c.id;
+
+SELECT pst.id
+FROM posts pst
+         join comments cmt
+              on pst.id = cmt.id;
+
+
+
+/*Связи:
+пользователь: кто оставил этот лайк;
+пост: какой пост лайкнули.
+*/
+
+SELECT u.id, u.name
+FROM users u
+         JOIN likes lk
+              ON u.id = lk.id_like;
+
+SELECT lk.id_like, lk.post_like
+FROM likes lk
+         JOIN posts pst
+             ON lk.id_like = pst.id;
+
+
+
+/*
+Связи:
+автор: пользователь, который подписался;
+подписка: пользователь, на которого подписались.*/
+
+SELECT sb.id, sb.who_signed_id
+FROM subscriber sb
+         JOIN users u
+              on sb.id = u.id;
+
+SELECT sb.id, sb.subscribed_id
+FROM subscriber sb
+         JOIN users u
+              on sb.id = u.id;
+
+
+/*Связи:
+
+отправитель: пользователь, отправивший сообщение;
+получатель: пользователь, которому отправили сообщение.*/
+
+SELECT ms.id_msg, ms.sender_id
+FROM messages ms
+         JOIN users u ON ms.id_msg = u.id;
+
+
+SELECT ms.id_msg, ms.recipient
+FROM messages ms
+         JOIN users u ON ms.id_msg = u.id;
+
+
+
+
 
 INSERT INTO content_type(title, class_icon)
-VALUES ('Цитата', 'quote');
-
-INSERT INTO content_type(title, class_icon)
-VALUES ('Картинка', 'photo');
-
-INSERT INTO content_type(title, class_icon)
-VALUES ('Видео', 'video');
-
-INSERT INTO content_type(title, class_icon)
-VALUES ('Ссылка', 'link');users
+VALUES ('Текст', 'text'),
+       ('Цитата', 'quote'),
+       ('Картинка', 'photo'),
+       ('Видео', 'video'),
+       ('Ссылка', 'link');
